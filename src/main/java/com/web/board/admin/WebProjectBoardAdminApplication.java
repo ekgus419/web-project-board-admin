b54ef43f758b26b753b3c43677d7644cc357d2bf
ekgus419 <ekgus419@gmail.com>
2024-07-11 22:32:18 +0900

    #4 - 리팩토링 : 들여쓰기를 탭에서 스페이스로 변경
    자동 완성 코드는 기본 값이 탭으로 확인
    들여쓰기는 스페이스를 쓰고 싶으므로 변경한다.

--- src/main/java/com/web/board/admin/WebProjectBoardAdminApplication.java
+++ src/main/java/com/web/board/admin/WebProjectBoardAdminApplication.java
@@ -8,8 +8,8 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 @SpringBootApplication
 public class WebProjectBoardAdminApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebProjectBoardAdminApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebProjectBoardAdminApplication.class, args);
+    }
 
 }
