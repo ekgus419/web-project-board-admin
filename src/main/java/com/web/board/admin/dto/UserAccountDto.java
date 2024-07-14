38adedc18768b24ab017b1ee418c2babbe00fe3e
ekgus419 <ekgus419@gmail.com>
2024-07-15 00:57:30 +0900

    #32 - 어드민 회원 테이블명 교체
    `user_account` -> `admin_account`

--- src/main/java/com/web/board/admin/dto/UserAccountDto.java
+++ src/main/java/com/web/board/admin/dto/UserAccountDto.java
@@ -1,6 +1,5 @@
 package com.web.board.admin.dto;
 
-import com.web.board.admin.domain.UserAccount;
 import com.web.board.admin.domain.constant.RoleType;
 
 import java.time.LocalDateTime;
@@ -8,7 +7,6 @@ import java.util.Set;
 
 public record UserAccountDto(
         String userId,
-        String userPassword,
         Set<RoleType> roleTypes,
         String email,
         String nickname,
@@ -19,40 +17,12 @@ public record UserAccountDto(
         String modifiedBy
 ) {
 
-    /** 메타 정보가 null 로 들어가는 생성자 */
-    public static UserAccountDto of(String userId, String userPassword, Set<RoleType> roleTypes, String email, String nickname, String memo) {
-        return UserAccountDto.of(userId, userPassword, roleTypes, email, nickname, memo, null, null, null, null);
+    public static UserAccountDto of(String userId, Set<RoleType> roleTypes, String email, String nickname, String memo) {
+        return UserAccountDto.of(userId, roleTypes, email, nickname, memo, null, null, null, null);
     }
 
-    /** 메타 정보가 세팅되는 생성자 (생성일시, 수정일시와 같은) */
-    public static UserAccountDto of(String userId, String userPassword, Set<RoleType> roleTypes, String email, String nickname, String memo, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
-        return new UserAccountDto(userId, userPassword, roleTypes, email, nickname, memo, createdAt, createdBy, modifiedAt, modifiedBy);
-    }
-
-    public static UserAccountDto from(UserAccount entity) {
-        return new UserAccountDto(
-                entity.getUserId(),
-                entity.getUserPassword(),
-                entity.getRoleTypes(),
-                entity.getEmail(),
-                entity.getNickname(),
-                entity.getMemo(),
-                entity.getCreatedAt(),
-                entity.getCreatedBy(),
-                entity.getModifiedAt(),
-                entity.getModifiedBy()
-        );
-    }
-
-    public UserAccount toEntity() {
-        return UserAccount.of(
-                userId,
-                userPassword,
-                roleTypes,
-                email,
-                nickname,
-                memo
-        );
+    public static UserAccountDto of(String userId, Set<RoleType> roleTypes, String email, String nickname, String memo, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
+        return new UserAccountDto(userId, roleTypes, email, nickname, memo, createdAt, createdBy, modifiedAt, modifiedBy);
     }
 
 }
--- src/main/java/com/web/board/admin/repository/UserAccountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.web.board.admin.repository;
-
-import com.web.board.admin.domain.UserAccount;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserAccountRepository extends JpaRepository<UserAccount, String> {
-}
